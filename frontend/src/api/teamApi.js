870a49fedc97d182e71cd479cf3d3128fbedd89f
Your Name <Nwaakhald@gmail.com>
2026-06-02 16:18:34 +0300

    frontend: add update/delete/getSessions to teamApi

--- frontend/src/api/teamApi.js
+++ frontend/src/api/teamApi.js
@@ -9,3 +9,18 @@ export const createTeamMember = async (data) => {
   const response = await api.post("/team-members", data);
   return response.data;
 };
+
+export const updateTeamMember = async (id, data) => {
+  const response = await api.put(`/team-members/${id}`, data);
+  return response.data;
+};
+
+export const deleteTeamMember = async (id) => {
+  const response = await api.delete(`/team-members/${id}`);
+  return response.data;
+};
+
+export const getTeamMemberSessions = async (id) => {
+  const response = await api.get(`/team-members/${id}/sessions`);
+  return response.data;
+};
